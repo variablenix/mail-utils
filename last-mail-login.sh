5247816419f45757a3fd4d70568750f7591c7641
TC <hello@kode.email>
2018-07-08 02:06:40 -0700

    Update code to show results

--- last-mail-login.sh
+++ last-mail-login.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Get the last login of a mail user
 # Perl source: https://stackoverflow.com/a/3178341/5575161
 
 maillog='/var/log/mail/mail.log'
 
-perl -ne '$l{$2}=$1 if /^(.{15}) .* imap-login: Login: user=<([^>]+)>/; END { print "$_ last imap-login: $l{$_}\n" for keys %l }' "$maillog"
+#perl -ne '$l{$2}=$1 if /^(.{15}) .* imap-login: Login: user=<([^>]+)>/; END { print "$_ last imap-login: $l{$_}\n" for keys %l }' "$maillog"
+perl -ne '$l{$2}=$1 if /^(.{15}) .* Login: user=<([^>]+)>/; END { print "$_ last imap-login: $l{$_}\n" for keys %l }' "$maillog"
