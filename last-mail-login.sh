d4c23056ddd07e9a1a0c4e49f4c41e1892af5757
TC <hello@kode.email>
2018-07-08 06:08:53 -0700

    Sort output

--- last-mail-login.sh
+++ last-mail-login.sh
@@ -6,4 +6,4 @@
 maillog='/var/log/mail/mail.log'
 
 #perl -ne '$l{$2}=$1 if /^(.{15}) .* imap-login: Login: user=<([^>]+)>/; END { print "$_ last imap-login: $l{$_}\n" for keys %l }' "$maillog"
-perl -ne '$l{$2}=$1 if /^(.{15}) .* Login: user=<([^>]+)>/; END { print "$_ last imap-login: $l{$_}\n" for keys %l }' "$maillog"
+perl -ne '$l{$2}=$1 if /^(.{15}) .* Login: user=<([^>]+)>/; END { print "$_ last imap-login: $l{$_}\n" for keys %l }' "$maillog" | sort -h
