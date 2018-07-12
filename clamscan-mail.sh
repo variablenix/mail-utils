1f400125f4380aa5311254a14221ea3a4073e799
TC <hello@kode.email>
2018-07-11 23:06:34 -0700

    Add phishing scan options and move infected mail to quarantine

--- clamscan-mail.sh
+++ clamscan-mail.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 #
-# Scan mail store and backups for dirty files
+# Scan mail store for infected mail and move to quarantine
+# info: https://www.lisenet.com/2014/automate-clamav-to-perform-daily-system-scan-and-send-email-notifications-on-linux/
 
 # check if we have admin rights
-[[ $EUID -ne 0 ]] && echo "I need permission to run" && exit 1
+[[ $EUID -ne 0 ]] && echo "root required" && exit 1
 
 DATE="$(date "+%Y-%m-%d %H:%M:%S%z")"
 LOG='/var/log/clamav/scan.log'
-EMAIL_MSG="ClamAV mail scan finished at $DATE. Please see attached scan log."
-EMAIL_FROM=clamav@example.net
-EMAIL_TO=admin@example.net
-_MAILSCAN=('/home/vmail' '/mnt/backups/mail')
+EMAIL_MSG="ClamAV mail scan finished on $DATE. Infected mail has been quarantined. Please see attached scan log for details."
+EMAIL_FROM=clamav@domain.net
+EMAIL_TO=admin@domain.net
+_MAILSCAN=('/home/vmail')
+QUAR='/tmp/clamscan/quarantine'
+
+# Create quarantine directory if it does not exist
+[[ -d "$QUAR" ]] || mkdir -p "$QUAR"
 
 # Scan mail store and mail backups
-(nice -n 5 clamscan -ri "${_MAILSCAN[@]}" --scan-mail=yes --phishing-sigs=yes --phishing-scan-urls=yes --max-filesize=42949672960 &> "$LOG";)
+(nice -n 5 clamscan -ri "${_MAILSCAN[@]}" --move="$QUAR" --scan-mail=yes --phishing-sigs=yes --phishing-scan-urls=yes --phishing-cloak=yes --max-filesize=42949672960 &> "$LOG";)
 
 # get the value of "Infected lines"
 MALWARE=$( tail "$LOG" | grep -i infected | cut -d" " -f3 );
 
 # if the value is not equal to zero, send an email with the log file attached
 [[ "$MALWARE" -ne 0 ]] && \
-	echo "$EMAIL_MSG" | mail -a "$LOG" -s "Malware Found" -r "$EMAIL_FROM" "$EMAIL_TO"
+	echo "$EMAIL_MSG" | mail -a "$LOG" -s "$HOSTNAME Mail Scan: Malware Found" -r "$EMAIL_FROM" "$EMAIL_TO"
 
 exit 0
